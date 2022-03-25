09429d65ebfeb5fdafbe22c020857ff8b62dcdf8
inzamulhaque <mdihalif@gmail.com>
2022-03-25 23:54:45 +0600

    show product

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -3,15 +3,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 import "./Product.css";
 
-const Product = () => {
+const Product = ({ product }) => {
+    const { name, price, image } = product;
     return (
         <div className='product'>
             <div className="productImage">
-                <img src="https://splendid-ganache-90bb08.netlify.app/images/k.jpg" alt="Product Image" />
+                <img src={image} alt="Product Image" />
             </div>
             <div className="productInfo">
-                <h3>Name: </h3>
-                <p>Price: </p>
+                <h3>Name: {name}</h3>
+                <p>Price: {price}</p>
                 <button className='addCartBtn'>Add To Cart <FontAwesomeIcon icon={faShoppingCart} /></button>
             </div>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Product from '../Product/Product';
 import "./Shop.css";
 
 const Shop = () => {
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+        fetch("data.json")
+            .then(res => res.json())
+            .then(data => setProducts(data));
+    }, []);
+
     return (
         <div className='shop'>
             <div className="productsContainer">
-                <Product />
-                <Product />
-                <Product />
+                {
+                    products.map(product => <Product key={product.id} product={product} />)
+                }
             </div>
 
             <div className="cartContainer">
