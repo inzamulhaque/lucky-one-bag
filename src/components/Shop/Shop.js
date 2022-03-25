194d607279c83940ec739e6f3d4d0b788550b89f
inzamulhaque <mdihalif@gmail.com>
2022-03-25 23:29:00 +0600

    create shop component

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "./Shop.css";
+
+const Shop = () => {
+    return (
+        <div className='shop'>
+            <div className="productsContainer">
+                products Container
+            </div>
+
+            <div className="cartContainer">
+                cart Container
+            </div>
+        </div>
+    );
+};
+
+export default Shop;
