93bea758db2ead307414537eace214a8740a63c3
inzamulhaque <mdihalif@gmail.com>
2022-03-25 22:59:20 +0600

    create header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Header.css";
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='siteTitle'>Max Shop</h1>
+            <p className='subTitle'>choose your favorite 4</p>
+        </div>
+    );
+};
+
+export default Header;
